d2fb91c4231ef29fd75620861e83cc3e0951901d
Pinbrawl <sans-andertale@mail.ru>
2024-12-27 13:08:58 +0400

    Refactoring Cube.cs

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -2,14 +2,15 @@ using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Renderer))]
+[RequireComponent(typeof(Rigidbody))]
 public class Cube : MonoBehaviour
 {
     [SerializeField] private Renderer _renderer;
 
+    public event Action<Cube> Clicked;
+
     [field:SerializeField] public int Chance {  get; private set; }
     public Rigidbody Rigidbody { get; private set; }
-
-    public event Action<Cube> Clicked;
 
     private void Awake()
     {
